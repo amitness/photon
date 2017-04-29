265d6e788365770492cbd8ac61fbcbc0a9e8dfae
Amit Chaudhary <meamitkc@gmail.com>
2017-04-29 09:20:42 +0545

    Add Conversion utility

---
+++ app/src/main/java/com/amitness/photon/utils/Conversion.java
@@ -0,0 +1,12 @@
+package com.amitness.photon.utils;
+
+/**
+ * Provides functions for message transformation
+ */
+
+public class Conversion {
+    public static byte[] toAscii(String text) {
+        return text.getBytes();
+    }
+
+}
