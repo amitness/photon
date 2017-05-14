86ae9b4a893e386172a95c9e7df48b85043e37b6
Amit Chaudhary <meamitkc@gmail.com>
2017-05-14 09:19:58 +0545

    Enable 3 dots overflow menu in all activities

--- app/src/main/java/com/amitness/photon/TransmitActivity.java
+++ app/src/main/java/com/amitness/photon/TransmitActivity.java
@@ -1,10 +1,13 @@
 package com.amitness.photon;
 
+import android.content.Intent;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.io.IOException;
@@ -18,6 +21,25 @@ public class TransmitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transmit);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.id_about) {
+            //Intent to another activity
+            Intent intentAbout=new Intent(TransmitActivity.this,AboutActivity.class);
+            startActivity(intentAbout);
+            return true;
+        }
+        return true;
+    }
+
 
     @SuppressWarnings("deprecation")
     public void startTransmission(View view) {
